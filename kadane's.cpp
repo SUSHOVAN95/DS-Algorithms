b2716a1a3536c865d2296cfba701d9b2da3c9e92
Sushovan Saha <31372586+ambideXtrous9@users.noreply.github.com>
2021-08-15 03:11:11 +0530

    Update kadane's.cpp

--- kadane's.cpp
+++ kadane's.cpp
@@ -14,14 +14,29 @@ int maxSubArraySum(int A[], int n)
 
 int maxSubArraySum(int a[], int n) 
 { 
+   int M = a[0];
+   int K = a[0]; 
+   int i;
+   for (i = 1; i < n; i++) 
+   { 
+        K = max(a[i],K+a[i]); // for storing the previous max;
+        M = max(M,K); // storing the actual max sum 
+   } 
+   return M;
+} 
+
+int maxSubArraySum(int a[], int n) 
+{ 
    int M[n]; 
-   M[0] = a[0]; 
+   M[0] = a[0];
+   int K = a[0];
   
    for (int i = 1; i < n; i++) 
    { 
-        M[i] = max(M[i-1], M[i-1]+a[i]); 
+	K = max(a[i],K+a[i]);
+        M[i] = max(M[i-1],K); 
    } 
-   return M[n];
+   return M[n-1];
 } 
 
 int main() 
